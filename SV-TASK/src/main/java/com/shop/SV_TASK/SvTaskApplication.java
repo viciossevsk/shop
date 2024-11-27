373abcf547f553ec3f9a163127d01cb9c4dc8632
viciossevsk <viciossevsk@gmail.com>
2024-11-28 00:32:56 +0300

    create project

---
+++ SV-TASK/src/main/java/com/shop/SV_TASK/SvTaskApplication.java
@@ -0,0 +1,13 @@
+package com.shop.SV_TASK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvTaskApplication.class, args);
+	}
+
+}
