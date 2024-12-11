e935ccb6c54a152f151222664e20c4cd519c3d02
viciossevsk <viciossevsk@gmail.com>
2024-12-12 02:31:35 +0300

    add EqualsAndHashCode

--- SV-TASK/src/main/java/com/shop/SV_TASK/domain/Supply.java
+++ SV-TASK/src/main/java/com/shop/SV_TASK/domain/Supply.java
@@ -36,9 +36,4 @@ public class Supply {
 
 
     Set<ProductPrice> productPrices = new HashSet<>();
-
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "productprice_id", referencedColumnName = "id")
-//    ProductPrice productPrice;
-
 }
